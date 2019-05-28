794399b07abf8cf13e7b8e97c61e8feeaef8192c
i338017 <pooja.khandhadia@sap.com>
2019-05-28 10:58:56 +0530

    UI done

---
+++ script.js
@@ -0,0 +1,50 @@
+class Calculator{
+    constructor(previousOperandTextElement, currentOperandTextElement){
+        this.previousOperandTextElement = previousOperandTextElement;
+        this.currentOperandTextElement = currentOperandTextElement;
+        this.clear()
+    }
+
+    clear(){
+        this.previousOperand = ''
+        this.currentOpernd = ''
+        this.operation = undefined
+    }
+
+    delete(){
+
+    }
+
+    appendNumber(number){
+
+    }
+
+    chooseOperation(operation){
+
+    }
+
+    compute(){
+
+    }
+
+    updateDisplay(){
+
+    }
+}
+
+
+const numberButtons = document.querySelectorAll('[data-number]')
+const operationButtons = document.querySelectorAll('[data-operation]')
+const equalsButtons = document.querySelectorAll('[data-equals]')
+const deleteButtons = document.querySelectorAll('[data-delete]')
+const allClearButtons = document.querySelectorAll('[data-all-clear]')
+const previousOperandTextElement = document.querySelectorAll('[data-previous-operand]')
+const currentOperandTextElement = document.querySelectorAll('[data-current-operand]')
+
+const calculator = new Calculator(previousOperandTextElement, currentOperandTextElement)
+
+numberButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        calculator.appendNumber(button.innerHTML);
+    })
+})
